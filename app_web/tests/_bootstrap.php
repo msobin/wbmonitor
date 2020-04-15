812f1c20178a041843ad0f7674ef4a72b5b5234d
Maksim Sobin <maxim.sobin@gmail.com>
2020-04-15 08:53:58 +0600

    Add tests

---
+++ app_web/tests/_bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: koktut
+ * Date: 02.04.20
+ * Time: 18:53
+ */
---
+++ app_web/tests/unit/_bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+define('YII_ENV', 'test');
+
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require __DIR__ . '/../vendor/autoload.php';
